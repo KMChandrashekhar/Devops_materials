ff7ec0dc0a79ef6938a237f0cadbfc4c5c347cee
kirancgwd <kirancgwd@gmail.com>
2025-08-19 15:46:25 +0530

    api

--- Python/Boto3/S3/File_Upload_S3.py
+++ Python/Boto3/S3/File_Upload_S3.py
@@ -4,7 +4,7 @@ import boto3
 s3 = boto3.client('s3')
 bucket_name = 'pbx-httpd-logs'
 file_path = 'httpd.log'
-
+API_KEY = 'hfkbddhededmekdhe'
 s3.upload_file(file_path, bucket_name, 'httpd')
 print(f"File {file_path} uploaded to bucket {bucket_name} as 'httpd'")  # Confirmation message
 
